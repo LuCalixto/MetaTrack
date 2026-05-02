92359edcbbb02162d1b06e93fd966122ac75ef8d
Luiza Calixto <lucalixto1130@gmail.com>
2026-05-01 23:53:10 -0300

    Atualizações e correções

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey:            "AIzaSyDNtC59j5VIY-mmznyjDUvZ1FkjQKgmf3o",
+    authDomain:        "ethania.firebaseapp.com",
+    projectId:         "ethania",
+    storageBucket:     "ethania.firebasestorage.app",
+    messagingSenderId: "290971711529",
+    appId:             "1:290971711529:web:655a835e54baeef36d4438",
+    measurementId:     "G-DZDTQ0B676"
+};
