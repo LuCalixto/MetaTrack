2a431c96504e547d5570686220d62de1ac8caf96
Luiza Calixto <lucalixto1130@gmail.com>
2026-05-09 15:55:56 -0300

    Atualização de carregamento de entrada na pagina

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey:            "AIzaSyDkXhVOjjsxhUG-edy320Ru4FaWVvyECm8",
+    authDomain:        "ethania.firebaseapp.com",
+    projectId:         "ethania",
+    storageBucket:     "ethania.firebasestorage.app",
+    messagingSenderId: "290971711529",
+    appId:             "1:290971711529:web:655a835e54baeef36d4438",
+    measurementId:     "G-DZDTQ0B676"
+};
