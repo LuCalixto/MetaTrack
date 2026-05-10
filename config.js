78d6277d3fa8091f0f456cb73d1f161e1268b0a3
Luiza Calixto <lucalixto1130@gmail.com>
2026-05-10 14:31:45 -0300

    resolução do login

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey:            "AIzaSyAArJr2ZMRuL8uhMc4rFHNeM1nzgPT3nhY",
+    authDomain:        "ethania.firebaseapp.com",
+    projectId:         "ethania",
+    storageBucket:     "ethania.firebasestorage.app",
+    messagingSenderId: "290971711529",
+    appId:             "1:290971711529:web:655a835e54baeef36d4438",
+    measurementId:     "G-DZDTQ0B676"
+};
